da5bee8f7db5eed4f05fc18c55f0f4ee99fa5367
Ali Mones <AliIsHomie@outlook.com>
2025-05-03 21:42:07 +0300

    refactored restaurant.launch.py (testing pending)

--- src/restaurant/launch/restaurant.launch.py
+++ src/restaurant/launch/restaurant.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -8,12 +9,25 @@ import os
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('neu_lidar'), 'launch', 'neu_lidar_launch.py')),
-        ),
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_test'), 'launch', 'navigation_launch.py')),
-        ),
+    is_sim = LaunchConfiguration("is_sim")
+    declare_is_sim = DeclareLaunchArgument(
+        'is_sim', default_value='true',
+        description='Set to true if running in simulation'
+    ),
+
+    neu_lidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('neu_lidar'), 'launch', 'neu_lidar_launch.py')),
+        launch_arguments=[('is_sim', is_sim)]
+    ),
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_test'), 'launch', 'navigation_launch.py')),
+        launch_arguments=[('use_sim_time', is_sim)]
+    ),
+
+    return LaunchDescription([
+        declare_is_sim,
+        neu_lidar_launch,
+        nav2_launch
     ])
