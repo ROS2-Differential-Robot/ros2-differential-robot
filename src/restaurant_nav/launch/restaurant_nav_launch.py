fc03718952366ba7cbbddf551405e7834dae46e1
AH-HESHAM <ahmedhesham1652001@gmail.com>
2025-05-12 16:20:03 +0300

    edit restaurant nav launch file to be suitable for real time situation

--- src/restaurant_nav/launch/restaurant_nav_launch.py
+++ src/restaurant_nav/launch/restaurant_nav_launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument, GroupAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.parameter_descriptions import ParameterValue
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessStart
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -12,14 +14,42 @@ import os
 
 def generate_launch_description():
     os.environ['GZ_SIM_RESOURCE_PATH'] = os.environ.get('GZ_SIM_RESOURCE_PATH', '') + f":{os.path.join(get_package_share_directory('restaurant'), 'models')}"
+
+    is_sim = LaunchConfiguration("is_sim")
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    controller_params = os.path.join(get_package_share_directory("neu_lidar"), "config", "controllers.yaml")
+    controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_cont', 'joint_broad']
+    )
+    
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description}, controller_params, {"use_sim_time": is_sim}],
+    )
+
+    delayed_controller_manager = TimerAction(
+            period=3.0,
+            actions=[controller_manager]
+    )
+
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_test'), 'launch', 'navigation_launch.py')),
+        DeclareLaunchArgument(
+            'is_sim', default_value='true',
+            description='Set to true if running in simulation'
         ),
+
+        # IncludeLaunchDescription(
+            # PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_test'), 'launch', 'navigation_launch.py')),
+        # ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
             launch_arguments=[('gz_args', "-r " + os.path.join(get_package_share_directory('restaurant'), 'world', 'restaurant.sdf'))],
+            condition=IfCondition(is_sim)
         ),
 
         Node(
@@ -28,7 +58,8 @@ def generate_launch_description():
             parameters=[{
                 'config_file': os.path.join(get_package_share_directory('neu_lidar'), 'config', 'gz_bridge_config.yaml'),
             }],
-            output='screen'
+            output='screen',
+            condition=IfCondition(is_sim)
         ),
 
         Node(
@@ -36,9 +67,9 @@ def generate_launch_description():
             executable='robot_state_publisher',
             parameters=[{
                 'robot_description': ParameterValue(
-                    Command(['xacro ', str(os.path.join(get_package_share_directory('neu_lidar'), 'model', 'v3_approx_xacro.urdf'))]), value_type=str
+                    Command(['xacro ', str(os.path.join(get_package_share_directory('neu_lidar'), 'model', 'v3_approx_xacro.urdf')),  " is_sim:=", is_sim]), value_type=str
                 ),
-                'use_sim_time': True,
+                'use_sim_time': is_sim,
             }],
             output='screen'
         ),
@@ -63,15 +94,67 @@ def generate_launch_description():
             arguments=['0', '0', '0', '0', '0', '0', 'lidar', 'adam/base_link/imu_sensor'],
             output='screen'
         ),
-        Node(
-            package="rplidar_ros",
-            executable="rplidar_composition",
-            parameters=[{
-                "serial_port": "/dev/ttyUSB0",
-                    "frame_id": "lidar",
-                    "angle_compensate": True
-            }]
+
+        GroupAction(
+            actions=[
+                delayed_controller_manager,
+
+                RegisterEventHandler(
+                    event_handler=OnProcessStart(
+                        target_action=controller_manager,
+                        on_start=[controller_spawner]
+                    )
+                ),
+
+                Node(
+                    package='robot_localization',
+                    executable='ekf_node',
+                    name='ekf_filter_node',
+                    output='screen',
+                    parameters=[
+                        os.path.join(get_package_share_directory('neu_lidar'), 'config', 'ekf-config.yaml'),
+                        {'use_sim_time': False}
+                    ],
+                ),
+
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')),
+                    launch_arguments=[
+                        ('use_sim_time', 'false'),
+                        ('slam_params_file', os.path.join(get_package_share_directory('restaurant_nav'), 'config', 'mapper_params_online_async.yaml')),
+                    ],
+                ),
+
+                Node(
+                    package='neu_lidar',
+                    executable='joystick_twist',
+                    output='screen',
+                ),
+
+                Node(
+                    package="rplidar_ros",
+                    executable="rplidar_composition",
+                    parameters=[{
+                        "serial_port": "/dev/ttyUSB0",
+                         "frame_id": "lidar",
+                         "angle_compensate": True
+                    }]
+                ),
+
+                Node(
+                    package='restaurant_nav',
+                    executable='pose_recorder',
+                ),
+                
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(get_package_share_directory('mpu6050'), 'launch', 'mpu6050.launch.py'))
+                ),                
+            ],
+            condition=UnlessCondition(is_sim)
         ),
+
         TimerAction(
             period=35.0,  # Timeout in seconds
             actions=[
@@ -100,11 +183,12 @@ def generate_launch_description():
                 ),
 
                 IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_test'), 'launch', 'localization_launch.py')),
+                    PythonLaunchDescriptionSource(
+                        os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')),
                     launch_arguments=[
-                        ("use_sim_time", "true"),
-                        ("map", os.path.join(get_package_share_directory("restaurant_nav"), "config", "restaurant_map.yaml"))
-                    ]
+                        ('use_sim_time', 'true'),
+                        ('slam_params_file', os.path.join(get_package_share_directory('restaurant_nav'), 'config', 'mapper_params_online_async.yaml')),
+                    ],
                 ),
 
                 Node(
@@ -134,8 +218,8 @@ def generate_launch_description():
                     package='restaurant_nav',
                     executable='publisher_table_num',
                 ),
-                
-            ]
+            ],
+            condition=IfCondition(is_sim)
         ),
         
     ])
